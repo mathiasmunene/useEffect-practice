0ea7e205b59f314f92f6c748e4c52b54f0de1072
Mathias Munene <munenemathias@gmail.com>
2025-05-14 09:59:55 +0300

    buttons added in search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,21 @@
-export default function Search() {
-    return (
-        <div className="search">
-            <input className="search-number" type="button" value="Get Random User"/>
-        </div>
-    )
+export default function Search({ url, setUrl }) {
+  const handleRandomUser = () => {
+    const randomId = Math.floor(Math.random() * 10) + 1; // Random number 1–10
+    setUrl(`https://jsonplaceholder.typicode.com/users/${randomId}`);
+  };
+
+  const handleAllUsers = () => {
+    setUrl("https://jsonplaceholder.typicode.com/users/"); // Reset to all users
+  };
+
+  return (
+    <div className="search">
+      <button className="search-number" onClick={handleRandomUser}>
+        Get Random User
+      </button>
+      <button className="all-users" onClick={handleAllUsers}>
+        Get All Users
+      </button>
+    </div>
+  );
 }
