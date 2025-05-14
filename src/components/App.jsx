a525c4a5cfc5724d2c07a68ab92a47374af8dbab
Mathias Munene <munenemathias@gmail.com>
2025-05-14 11:17:51 +0300

    finalizing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,13 @@
 import UserList from "./UserList";
 import { useState, useEffect } from "react";
 
+/**We need to:
+
+- Add a state for the URL.
+- Update useEffect to fetch data based on the URL.
+- Pass the URL and a function to update it to UserList. */
+
+
 function App() {
   const [users, setUsers] = useState([]);
   const [url, setUrl] = useState("https://jsonplaceholder.typicode.com/users/"); // State for URL
@@ -12,7 +19,10 @@ function App() {
         // Ensure data is always an array for consistent rendering
         setUsers(Array.isArray(data) ? data : [data]);
       })
+
       .catch((error) => console.error("Error fetching users:", error));
+      // not neccessary but It catches any failures in the fetch request chain
+
   }, [url]); // URL as dependency
 
   return (
