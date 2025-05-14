b933e3cb80eea2ed6cfca7a336d263a97c9e0435
Mathias Munene <munenemathias@gmail.com>
2025-05-14 10:02:18 +0300

    UserCard.jsx simplified

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,21 +1,21 @@
 export default function Search({ url, setUrl }) {
-  const handleRandomUser = () => {
-    const randomId = Math.floor(Math.random() * 10) + 1; // Random number 1–10
-    setUrl(`https://jsonplaceholder.typicode.com/users/${randomId}`);
-  };
-
-  const handleAllUsers = () => {
-    setUrl("https://jsonplaceholder.typicode.com/users/"); // Reset to all users
-  };
-
-  return (
-    <div className="search">
-      <button className="search-number" onClick={handleRandomUser}>
-        Get Random User
-      </button>
-      <button className="all-users" onClick={handleAllUsers}>
-        Get All Users
-      </button>
-    </div>
-  );
-}
+    const handleRandomUser = () => {
+      const randomId = Math.floor(Math.random() * 10) + 1; // Random number 1–10
+      setUrl(`https://jsonplaceholder.typicode.com/users/${randomId}`);
+    };
+  
+    const handleAllUsers = () => {
+      setUrl("https://jsonplaceholder.typicode.com/users/"); // Reset to all users
+    };
+  
+    return (
+      <div className="search">
+        <button className="search-number" onClick={handleRandomUser}>
+          Get Random User
+        </button>
+        <button className="all-users" onClick={handleAllUsers}>
+          Get All Users
+        </button>
+      </div>
+    );
+  }
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,12 +1,15 @@
-export default function UserCard(user) {
+export default function UserCard({ username, name, email }) {
     return (
-        <>
-            <div className="user-card">
-                <img width="100" height="100" src="https://img.icons8.com/material-rounded/100/user-male-circle.png" alt="user-male-circle"/>
-                <h2 className="user-name">{user.username}</h2>
-                <p className="user-role">{user.name}</p>
-                <p className="user-description">{user.email}</p>
-            </div>
-        </>
-    )
-}
+      <div className="user-card">
+        <img
+          width="100"
+          height="100"
+          src="https://img.icons8.com/material-rounded/100/user-male-circle.png"
+          alt="user-male-circle"
+        />
+        <h2 className="user-name">{username}</h2>
+        <p className="user-role">{name}</p>
+        <p className="user-description">{email}</p>
+      </div>
+    );
+  }
