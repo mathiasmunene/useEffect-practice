84f6b88f01a9f86442f054ffdf2006bae18e62cc
Mathias Munene <munenemathias@gmail.com>
2025-05-14 10:05:09 +0300

    render a single user or the full list

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,14 +1,20 @@
-import UserCard from './UserCard'
-import Search from './Search'
+import UserCard from "./UserCard";
+import Search from "./Search";
 
-export default function UserList({users}) {
+export default function UserList({ users, url, setUrl }) {
+  // Check if URL points to a single user (contains an ID)
+  const isSingleUser = url.includes("/users/") && url.split("/users/")[1];
 
-    return (
-        <>
-            <Search />
-            <div className="user-list">
-                {users.map((user) => UserCard(user))}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Search url={url} setUrl={setUrl} />
+      <div className="user-list">
+        {isSingleUser ? (
+          users.length > 0 && <UserCard {...users[0]} />
+        ) : (
+          users.map((user) => <UserCard key={user.id} {...user} />)
+        )}
+      </div>
+    </>
+  );
 }
